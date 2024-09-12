6e905f0f830cf51893742129cd48742a6c33e797
Aibak A Aljadayh <65025788+aibak365@users.noreply.github.com>
2024-09-12 19:14:01 -0400

    Schema

---
+++ Homework2/Homework_2_Aibak_Aljadayah.rkt
@@ -0,0 +1,38 @@
+#lang racket
+(define (mydisplay value)
+  (display value)
+  (newline)
+  )
+
+;An example of mydisplay function
+(mydisplay 3)
+;End of example
+
+(define (line func)
+  (display "----------")
+  (display func)
+  (display "----------")
+  (newline)
+  )
+
+;An example of line function
+(line (+ 3 5))
+;End of example
+
+(define (negatives lst)
+  (cond
+    ((null? lst) '())
+    (else
+     (if (< (car lst) 0)
+         (cons (car lst) (negatives (cdr lst)))
+         (negatives (cdr lst))
+     )
+    )
+  )
+ )
+;An example of line function
+(mydisplay (negatives '())) ; -> ()
+(mydisplay (negatives '(-1))) ; -> (-1)
+(mydisplay (negatives '(1 -1 2 3 4 -4 5))) ; -> (-1 -4)
+(mydisplay (negatives '(1 1 2 3 4 4 5))) ; -> ()
+;End of example
