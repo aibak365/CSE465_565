67340163401a428b8506aadce685cf6192f5c4af
Aibak365 <2019904038@ses.yu.edu.jo>
2024-09-23 22:51:30 -0400

    new functions

--- Homework2/Homework_2_Aibak_Aljadayah.rkt
+++ Homework2/Homework_2_Aibak_Aljadayah.rkt
@@ -40,19 +40,21 @@
 
 (define (struct lst1 lst2)
   (cond
-    ((and (null? lst1) (null? lst2)) #t)
-    ((or (null? lst1) (null? lst2)) #f) 
-    (
-     (and (pair? (car lst1)) (pair? (car lst2))) 
-     (and (struct (car lst1) (car lst2)) (struct (cdr lst1) (cdr lst2)))
-    )
-    ((or (pair? (car lst1)) (pair? (car lst2))) #f) 
-    (else (struct (cdr lst1) (cdr lst2))) 
+  ((and (null? lst1) (null? lst2))#t)
+  ((or (null? lst1)(null? lst2))#f)
+  (
+   (and (list? (car lst1)) (list? (car lst2))) 
+   (and (struct (car lst1) (car lst2)) (struct (cdr lst1) (cdr lst2)))
+   )
+  ((or (list? (car lst1)) (list? (car lst2)))#f)
+  (else (struct (cdr lst1) (cdr lst2)))
   )
 )
 
 ;An example of line function
 (line "struct")
+(mydisplay (struct '() '())) ; -> #t
+(mydisplay (struct '(12) '())) ; -> #f
 (mydisplay (struct '(a b c (c a b) (1 2 a)) '(1 2 3 (a b c) (1 2 c)))) ; -> #t
 (mydisplay (struct '(a b c d (c a b)) '(1 2 3 (a b c)))) ; -> #f
 (mydisplay (struct '(a b c (c a b)) '(1 2 3 (a b c) 0))) ; -> #f
@@ -75,6 +77,22 @@
 (line "minAndMax")
 (mydisplay (minAndMax '(1 2 -3 4 2))) ; -> (-3 4)
 (mydisplay (minAndMax '(1))) ; -> (1 1)
+(mydisplay (minAndMax '(1 2 3 4 0))) ; -> (0 4)
 (line "minAndMax")
 ;End of example
+
+(define (flatten lst)
+  (cond
+    ((null? lst)'())
+    ((list? (car lst)) (append (flatten (car lst)) (flatten (cdr lst))))
+    ((cons (car lst) (flatten(cdr lst))))
+  )
+)
+
+(line "flatten")
+(mydisplay (flatten '(a b c)))  ; -> (a b c)
+(mydisplay (flatten '(a (a a) a)))  ; -> (a a a a)
+(mydisplay (flatten '((a b) (c (d) e) f)))  ; -> (a b c d e f)
+(line "flatten")
+; ---------------------------------------------
 
