544c67af3e9316265ada7c2627edc6ba920794ee
Aibak365 <2019904038@ses.yu.edu.jo>
2024-11-14 11:29:18 -0500

    using pandas

--- Homework#5/hw5.py
+++ Homework#5/hw5.py
@@ -1,33 +1,35 @@
 import time
+from geopy.distance import geodesic # please don't forget to install it; pip install geopy
+import pandas as pd
+import numpy as np
+from scipy.spatial import cKDTree # as i understood is a better way of the memory
+ # please don't forget to install scipy
 
 """
-  Homework#5
-
-  Add your name here: ----
-
-  You are free to create as many classes within the hw5.py file or across 
-  multiple files as you need. However, ensure that the hw5.py file is the 
-  only one that contains a __main__ method. This specific setup is crucial 
-  because your instructor will run the hw5.py file to execute and evaluate 
-  your work.
-
+Homework#5
+Add your name here: Aibak Aljadayah
+You are free to create as many classes within the hw5.py file or across
+multiple files as you need. However, ensure that the hw5.py file is the
+only one that contains a __main__ method. This specific setup is crucial
+because your instructor will run the hw5.py file to execute and evaluate
+your work.
 """
 """
-1- => Lambda was used in line/s: ............
+1- => Lambda was used in line/s: 53
 2- => Filter or map was used in line/s: ...........
 3- => yield was used in line/s: .........
 """
-
-if __name__ == "__main__": 
-    start_time = time.perf_counter()  # Do not remove this line
+if __name__ == "__main__":
+    start_time = time.perf_counter() # Do not remove this line
     '''
     Inisde the __main__, do not add any codes before this line.
     -----------------------------------------------------------
     '''
+    # write your code here
+    # to read the file as panda dataframe
+    df = pd.read_csv("zipcodes.txt", delimiter='\t')
 
     
-    # write your code here
-
 
     '''
     Inside the __main__, do not add any codes after this line.
@@ -36,6 +38,4 @@ if __name__ == "__main__":
     end_time = time.perf_counter()
     # Calculate the runtime in milliseconds
     runtime_ms = (end_time - start_time) * 1000
-    print(f"The runtime of the program is {runtime_ms} milliseconds.")  
-    
-
+    print(f"The runtime of the program is {runtime_ms} milliseconds.")
